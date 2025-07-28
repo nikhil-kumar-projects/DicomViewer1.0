52b71f7a3864a7cb5acd606923b5e46a485e71f1
Kumar, Nikhil <nikhil.nk.kumar.ext@siemens-healthineers.com>
2025-07-28 16:43:57 +0200

    First Commit

--- WebApi/webApi.Api/Program.cs
+++ WebApi/webApi.Api/Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
